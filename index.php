2f93ff075e9b82e45b27952eb72e87179cc8d329
Charles Sandidge <sandidgec@gmail.com>
2016-06-29 09:13:30 -0600

    first commit. added file structure and index template

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php require_once("lib/headUtils.php"); ?>
+    <title>A.R.T. 30 for 30</title>
+</head>
+<body>
+    <h1>Test</h1>
+</body>
+</html>
---
+++ lib/headUtils.php
@@ -0,0 +1,12 @@
+<meta charset="UTF-8">
+
+<link href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" rel="stylesheet"
+      integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+
+<link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.6.3/css/font-awesome.min.css" rel="stylesheet" type="text/css">
+
+<link href='https://fonts.googleapis.com/css?family=Fjalla+One|Orbitron' rel='stylesheet' type='text/css'>
+
+<link rel="stylesheet" href="lib/css/style.css">
+
+<link href='https://fonts.googleapis.com/css?family=Dosis' rel='stylesheet' type='text/css'>
