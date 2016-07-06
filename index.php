ff7f19b157272a9e5ade77412e29a882e98cdb75
Charles Sandidge <sandidgec@gmail.com>
2016-07-06 16:20:09 -0600

    Merge branch 'master' of /Applications/XAMPP/htdocs/AtomSites/cc30 with conflicts.

--- index.php
+++ index.php
@@ -5,10 +5,38 @@
     <title>ABQ 30 in 30</title>
 </head>
 <body>
+
+<nav class="navbar navbar-fixed-top" id="FCF">
+    <div class="container-fluid">
+        <!-- Brand and toggle get grouped for better mobile display -->
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="https://twitter.com/CultivateCoders"><i class="fa fa-twitter" aria-hidden="true"></i></a>
+            <a class="navbar-brand" href="https://www.facebook.com/cultivatingcoders/?fref=ts&ref=br_tf"><i class="fa fa-facebook" aria-hidden="true"></i></a>
+        </div>
+
+        <!-- Collect the nav links, forms, and other content for toggling -->
+        <!--<div class="collapse navbar-collapse">
+            <ul class="nav navbar-nav">
+                <li><a href="#">Faster</a></li>
+                <li><a href="#">Casual</a></li>
+                <li><a href="#">Finer</a></li>
+                <li><a href="About.php">About Us</a></li>
+                <li><a href="contact.php">Contact</a></li>
+
+            </ul>
+        </div><!-- /.navbar-collapse -->
+    </div><!-- /.container-fluid -->
+</nav>
 <div class="jumbotron">
 
     <div class="col-md-6 col-md-offset-3">
-        <h2 class="appopen">Applications Open 7/6/16. Stay Tuned </h2>
+        <h2 class="appopen">Applications Open 7/6/16 @ 6 pm. Stay Tuned </h2>
     </div>
 
 </div>
